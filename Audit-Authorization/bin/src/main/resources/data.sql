aa559269f8152f72a6c5bfc392546f6f4cf68928
DRoy7 <46459210+DRoy7@users.noreply.github.com>
2021-07-22 17:49:16 +0530

    First Commit from Branch

---
+++ Audit-Authorization/bin/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO project_manager(id, name, username, password) VALUES (1, 'deep', 'dr7', '1234');
+INSERT INTO project_manager(id, name, username, password) VALUES (2, 'sri', 'flash', '12345');
+INSERT INTO project_manager(id, name, username, password) VALUES (3, 'kamalesh', 'code', 'code');

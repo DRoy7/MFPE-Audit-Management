d413b56cc74da82d71e6bf1cbf633cd30b3554d7
DRoy7 <46459210+DRoy7@users.noreply.github.com>
2021-08-02 20:30:09 +0530

    :)

--- ExecuteAll/Exec/RunWebApp.bat
+++ ExecuteAll/Exec/RunWebApp.bat
@@ -2,6 +2,6 @@ cd..
 cd..
 cd Audit-Management-App
 cd src
-xcopy app/ "D:/Cognizant Internship/My WorkSpace/MFPE/ProjectUI/Audit-Management-App/src/app/"  /E 
+xcopy app "D:/Cognizant Internship/My WorkSpace/MFPE/ProjectUI/Audit-Management-App/src/app/"  /E 
 cd "D:/Cognizant Internship/My WorkSpace/MFPE/ProjectUI/Audit-Management-App"
 ng serve --open --port 8400
--- ExecuteAll/ExecuteProject.bat
+++ ExecuteAll/ExecuteProject.bat
@@ -4,5 +4,4 @@ start RunAuthMS.bat
 start RunBenchMS.bat
 start RunCheckMS.bat
 start RunSeverityMS.bat
-start chrome --new-window "localhost:8300/db"
 start RunWebApp.bat

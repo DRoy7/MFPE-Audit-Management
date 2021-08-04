fb1d6a101126d64910f77c4af3c142660912de29
DRoy7 <46459210+DRoy7@users.noreply.github.com>
2021-08-04 13:03:25 +0530

    Thats it!

--- ExecuteAll/Exec/RunWebApp.bat
+++ ExecuteAll/Exec/RunWebApp.bat
@@ -4,5 +4,4 @@ cd Audit-Management-App
 cd src
 xcopy app "D:/Cognizant Internship/My WorkSpace/MFPE/ProjectUI/Audit-Management-App/src/app/"  /E 
 cd "D:/Cognizant Internship/My WorkSpace/MFPE/ProjectUI/Audit-Management-App"
-ng version
 ng serve --open --port 8400
--- ExecuteAll/MakeJars.bat
+++ ExecuteAll/MakeJars.bat
@@ -1,6 +1,6 @@
 echo "Making Audit-Management-System JARs\n"
 cd MakeJar
 start MakeJarAuthMS.bat
-start MakeJarMS.bat
-start MakeJarMS.bat
-start MakeJarMS.bat
+start MakeJarBenchMS.bat
+start MakeJarCheckMS.bat
+start MakeJarSeverityMS.bat

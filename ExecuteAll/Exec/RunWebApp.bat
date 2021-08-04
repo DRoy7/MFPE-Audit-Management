17c764cd150c1d7b2327d78af45a231ffde6592d
DRoy7 <46459210+DRoy7@users.noreply.github.com>
2021-08-04 19:16:46 +0530

    Gg

--- ExecuteAll/Exec/RunWebApp.bat
+++ ExecuteAll/Exec/RunWebApp.bat
@@ -2,6 +2,5 @@ cd..
 cd..
 cd Audit-Management-App
 cd src
-xcopy app "D:/Cognizant Internship/My WorkSpace/MFPE/ProjectUI/Audit-Management-App/src/app/"  /E 
-cd "D:/Cognizant Internship/My WorkSpace/MFPE/ProjectUI/Audit-Management-App"
+npm update
 ng serve --open --port 8400
